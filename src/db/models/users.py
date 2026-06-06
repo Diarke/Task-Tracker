94338e14c874378a83c53bea343337e9b119826d
Diarke <kerimkul_diar99@mail.ru>
2026-06-06 20:57:33 +0500

    refactor: rename User models

--- src/db/models/users.py
+++ src/db/models/users.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm import Mapped, mapped_column
 from src.db.base import Base
 
 
-class Users(Base):
+class UsersORM(Base):
     __tablename__="users"
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
