9613c6ee2af40d2d1601d3161755a1ad1e8a169e
Diarke <kerimkul_diar99@mail.ru>
2026-06-06 20:39:15 +0500

    feat: add User schema

--- src/schemas/users.py
+++ src/schemas/users.py
@@ -1,9 +1,18 @@
-from pydantic import BaseModel, EmailStr, SecretStr, field_validator
+from datetime import datetime
+
+from typing import Annotated
+from pydantic import (
+    BaseModel,
+    EmailStr,
+    SecretStr,
+    field_validator,
+    Field,
+)
 
 
 class CreateUser(BaseModel):
-    email: EmailStr
-    password: SecretStr
+    email: Annotated[EmailStr, Field(description="User email address")]
+    password: Annotated[SecretStr, Field(description="User password")]
 
     @field_validator("password")
     @classmethod
@@ -11,3 +20,11 @@ class CreateUser(BaseModel):
         if len(v.get_secret_value().strip()) < 8:
             raise ValueError("Password must be at least 8 characters long")
         return v
+
+
+class User(BaseModel):
+    id: Annotated[int, Field(description="Unique user identifier")]
+    email: Annotated[EmailStr, Field(description="User email address")]
+    hashed_password: Annotated[str, Field(description="Hashed user password")]
+    created_at: Annotated[datetime, Field(description="Timestamp when the user was created")]
+    updated_at: Annotated[datetime, Field(description="Timestamp when the user was last updated")]
