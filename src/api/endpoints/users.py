7d5f40dbbd7116d6fd162e6a5f0f7bdd5f0d380b
Diarke <kerimkul_diar99@mail.ru>
2026-06-08 18:22:38 +0500

    refactor: add response schema

--- src/api/endpoints/users.py
+++ src/api/endpoints/users.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, status
 
-from src.api.schemas.users import UserCreateRequest
+from src.api.schemas.users import UserCreateRequest, UserCreateResponse
 from src.api.dependencies.db import DBDep
 from src.services.users import UserService
 
@@ -12,6 +12,7 @@ router = APIRouter()
     "/",
     status_code=status.HTTP_201_CREATED,
     summary="create user",
+    response_model=UserCreateResponse
 )
 async def register(user: UserCreateRequest, db: DBDep):
     return await UserService(db).create_user(user)
