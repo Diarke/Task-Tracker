fdeec4813a359760cf58073ff6e09e0cfa63469c
Diarke <kerimkul_diar99@mail.ru>
2026-06-09 16:30:23 +0500

    feat: add new fields into UsersORM

--- src/db/models/users.py
+++ src/db/models/users.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from sqlalchemy import Integer, String, DateTime, func
+from sqlalchemy import Integer, String, DateTime, Boolean, func
 from sqlalchemy.orm import Mapped, mapped_column
 
 from src.db.base import Base
@@ -12,5 +12,7 @@ class UsersORM(Base):
     id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
     email: Mapped[str] = mapped_column(String(200), unique=True)
     hashed_password: Mapped[str] = mapped_column(String(200))
+    is_admin: Mapped[bool] = mapped_column(Boolean, default=False)
+    is_active: Mapped[bool] = mapped_column(Boolean, default=True)
     created_at: Mapped[datetime] = mapped_column(DateTime, default=func.now())
     updated_at: Mapped[datetime] = mapped_column(DateTime, default=func.now(), onupdate=func.now())
